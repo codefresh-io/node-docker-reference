a31bb0bb4d928f81fcbfcefb7892f59dc504f0e6
pashavictorovich <pavel@codefresh.io>
2021-07-30 21:07:32 +0300

    expose new optimized endpoint

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const { Reference }                                       = require('./lib/reference');
-const { parseQualifiedName, parseFamiliarName, parseAll } = require('./lib/parsers');
+const { parseQualifiedName, parseQualifiedNameOptimized, parseFamiliarName, parseAll } = require('./lib/parsers');
 
 Object.assign(exports, {
     Reference,
     parseQualifiedName,
+    parseQualifiedNameOptimized,
     parseFamiliarName,
     parseAll
 });

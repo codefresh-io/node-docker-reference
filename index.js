f6dbb39ac897902fc152779537812e439e0dcab0
oleksandr-codefresh <oleksandr.saulyak@codefresh.io>
2021-12-10 20:00:29 +0200

    added typescript definitions for published version

--- index.js
+++ index.js
@@ -3,10 +3,9 @@
 const { Reference }                                       = require('./lib/reference');
 const { parseQualifiedName, parseQualifiedNameOptimized, parseFamiliarName, parseAll } = require('./lib/parsers');
 
-Object.assign(exports, {
-    Reference,
-    parseQualifiedName,
-    parseQualifiedNameOptimized,
-    parseFamiliarName,
-    parseAll
-});
+// please export like this to work fine with typescript definitions
+exports.Reference = Reference;
+exports.parseQualifiedName = parseQualifiedName;
+exports.parseQualifiedNameOptimized = parseQualifiedNameOptimized;
+exports.parseFamiliarName = parseFamiliarName;
+exports.parseAll = parseAll;
